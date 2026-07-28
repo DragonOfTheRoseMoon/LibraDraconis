2ca45b99ccdda61d62ba737302f6314a98224b2c
DragonOfTheRoseMoon <malicedragon99@gmail.com>
2026-07-27 20:25:06 -0400

    Google API request by ISBN complete with dataclass structure

--- backend/formats.py
+++ backend/formats.py
@@ -1,33 +1,18 @@
 from dataclasses import dataclass, field
 
 
-
-# REFACTOR THIS FOR NEW SCHEMA
-
-@dataclass
-class BookOLD:
-    isbn: int  = 0
-    title: str = ""
-    authors: str = ""
-    series: str = ""
-    publisher: str = ""
-    publish_year: int = 0
-    pages: int = 0
-
-
 @dataclass
 class Book:
-    isbn: int  = 0
+    isbn: str  = ""
     title: str = ""
-    authors: str = ""
+    author: str = ""
     series: str = ""
-    series_position: int = 0
+    seriesPOS: int = 0
     publisher: str = ""
-    publish_year: int = 0
-    print_type: str = ""
+    publishYear: int = 0
     pages: int = 0
     thumbnail: str = ""
     small_thumbnail: str = ""
     source: str = ""
-    sourceid: str = ""
+    sourceID: str = ""
 
